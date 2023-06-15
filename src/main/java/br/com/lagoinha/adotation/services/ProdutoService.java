2cc9d456e210cf077e89ef9a116a3b26a57914f8
ViniAmaralAndrade <vini.amaralgfa@gmail.com>
2023-06-15 19:29:40 -0300

    melhorando query de ProdutoRepository

--- src/main/java/br/com/lagoinha/adotation/services/ProdutoService.java
+++ src/main/java/br/com/lagoinha/adotation/services/ProdutoService.java
@@ -1,30 +1,46 @@
 package br.com.lagoinha.adotation.services;
 
 import br.com.lagoinha.adotation.entities.Produto;
+import br.com.lagoinha.adotation.repositories.EstoqueRepository;
 import br.com.lagoinha.adotation.repositories.ProdutoRepository;
-import br.com.lagoinha.adotation.services.interfaces.ProdutoService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 @Service
-public class ProdutoServiceImpl implements ProdutoService {
+public class ProdutoServiceImpl {
 
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    @Override
+    @Autowired
+    private EstoqueRepository estoqueRepository;
+
+    @Autowired
+    private EstoqueService estoqueService;
+
     public List<Produto> mostrarTodos() {
         return produtoRepository.findAll();
     }
 
-    @Override
-    public Produto salvar(Produto produto) {
+    public boolean existePorEstoque(String nomeEstoque) {
+        if(estoqueRepository.existsByNomeIgnoreCase(nomeEstoque)){
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+    public Produto salvar(Produto produto) throws Exception {
+
+        if(produto.getId() != null){
+            buscarPorId(produto.getId());
+        }
 
         return this.produtoRepository.save(produto);
     }
@@ -39,7 +55,7 @@ public class ProdutoServiceImpl implements ProdutoService {
         return null;
     }
 
-    @Override
+
     public Produto buscarPorId(Long id) {
         Optional<Produto> produtoPesquisado = this.produtoRepository.findById(id);
 
@@ -53,7 +69,7 @@ public class ProdutoServiceImpl implements ProdutoService {
         return this.produtoRepository.findByProdutoIgnoreCase(produto);
     }
 
-    @Override
+
     public boolean removerPorId(Long id) {
         try{
             Produto produtoPesquisado = buscarPorId(id);
