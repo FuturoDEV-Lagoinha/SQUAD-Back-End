de49b54b7e7337b9cea2a05abbf89e2f4da138f5
ViniAmaralAndrade <vini.amaralgfa@gmail.com>
2023-06-15 21:56:54 -0300

    uns testes

--- src/main/java/br/com/lagoinha/adotation/repositories/UsuarioRepository.java
+++ src/main/java/br/com/lagoinha/adotation/repositories/UsuarioRepository.java
@@ -2,6 +2,9 @@ package br.com.lagoinha.adotation.repositories;
 
 import br.com.lagoinha.adotation.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
